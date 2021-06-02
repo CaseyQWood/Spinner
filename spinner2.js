0112fa63fcfa19656adbe96778d341974e916b62
Casey Wood <caseywood252@gmail.com>
2021-06-02 01:37:46 +0000

    refactored

--- spinner2.js
+++ spinner2.js
@@ -2,10 +2,8 @@ const slash = ['\r|', '\r/', '\r-', '\r\\', ];
 let delay = 0;
 
 const spinner = () => {
-  for (let i = 0; i < slash.length * 2; i++) {
-    delay += 250;
-    setTimeout(() => {process.stdout.write(slash[i])}, delay)
-  }
+  for (const hand of slash) {
+  setTimeout(() => {process.stdout.write(hand)}, delay += 250)}
 };
 
 spinner();
